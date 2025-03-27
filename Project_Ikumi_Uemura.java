9996b8887b13f9521fbd5fb77c0d722bf6c2c409
Ikumi333 <uemuraikumi0531@gmail.com>
2025-03-27 18:07:52 -0400

    Added code for demo class

--- Project_Ikumi_Uemura.java
+++ Project_Ikumi_Uemura.java
@@ -17,6 +17,7 @@ public class Project_Ikumi_Uemura
    System.out.print("Please enter the Policy Number: ");
    number = keyboard.nextInt();
    keyboard.nextLine();
+   
    System.out.print("Please enter the Provider Name: ");
    name = keyboard.nextLine();
    System.out.print("Please enter the Policyholder7s First Name: ");
@@ -28,6 +29,7 @@ public class Project_Ikumi_Uemura
    System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
    smoking = keyboard.nextLine();
    keyboard.nextLine();
+   
    System.out.print("Please enter the Policyholder's Height(in inches): ");
    height = keyboard.nextDouble();
    System.out.print("Please enter the Policyholder's Weight(in inches): ");
